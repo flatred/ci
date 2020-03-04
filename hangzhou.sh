b2ff4d51e8b0d57a67ba883c079f3015871e8729
pingh <pingh@getui.com>
2020-03-04 09:54:35 +0800

    change weather format

--- hangzhou.sh
+++ hangzhou.sh
@@ -4,11 +4,12 @@ set -eux
 
 CITY="杭州"
 LANGUAGE="zh-CN"
-UNIT="M"
+FORMAT="%l:+%c+%t+%w+%m"
+UNIT="m"
 UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o hangzhou.html \
-  "wttr.in/$CITY?format=4&$UNIT"
+  "wttr.in/$CITY?format=$FORMAT"
