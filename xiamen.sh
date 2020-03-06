e0822bb1dee627ca398455bc9812e1db1f679e15
pingh <pingh@getui.com>
2020-03-06 10:04:19 +0800

    refactor: rollback first version

--- xiamen.sh
+++ xiamen.sh
@@ -2,14 +2,14 @@
 
 set -eux
 
-CITY="Xiamen"
+CITY=Xiamen
 LANGUAGE="zh-CN"
 FORMAT="%l:+%c+%t+%w+%m"
-UNIT="M"
+UNIT=m
 UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o xiamen.html \
-  wttr.in/$CITY?format=$FORMAT
+  wttr.in/$CITY?format=4\&$UNIT
