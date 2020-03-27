78d0a0a26a8941af658ac33e49e230ca50d67770
pingh <pingh@getui.com>
2020-03-27 09:52:49 +0800

    feat: add cat report.html

--- report.sh
+++ report.sh
@@ -10,20 +10,22 @@ FORMAT="%l:+%c+%t+%w+%m"
 UNIT=m
 UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
 
-curl \
+curl -s \
   -w '\n' \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   wttr.in/$CITY1?format=4\&$UNIT > report.html
 
-curl \
+curl -s \
   -w '\n' \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   wttr.in/$CITY2?format=4\&$UNIT >> report.html
 
-curl \
+curl -s \
   -w '\n' \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   wttr.in/$CITY3?format=4\&$UNIT >> report.html
+
+cat report.html
